8d034cd6e869c7859078a0e09d9f890ecf6abc20
Debojyoti Dey <121328928+Deysdeveloper@users.noreply.github.com>
2026-08-15 20:53:52 +0530

    changed version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.Deysdeveloper.dailyvoicejournalapp"
         minSdk = 31
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3"
+        versionCode = 7
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
